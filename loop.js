002363595aa568f718f3d49eb9a81afab736d1a2
sanikalaanilkumar@gmail.com <sanikalaanilkumar@gmail.com>
2024-10-24 00:31:40 +0530

    loops files are added

---
+++ loop.js
@@ -0,0 +1,38 @@
+
+// 1. Printing Numbers from 1 to 10
+
+var a=1;
+for(a=1; a<=10; a++){
+    console.log(a);
+}
+
+var a = 1;
+while (a <= 10) {
+    console.log(a);
+    a++;
+}
+
+var a=1;
+do{
+console.log(a);
+a++;
+}while(a<=10)
+
+
+// 2. Printing 1 to 5 numbers in reverse order
+
+for(var a=5; a>=1; a--){
+    console.log(a);
+}
+
+var a=5;
+while(a>=1){
+console.log(a);
+a--;
+}
+
+var a=5;
+do{
+console.log(a);
+a--;
+}while(a>=1)
